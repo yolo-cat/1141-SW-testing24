9bf2c474064ecc62b1dcc1b83cd976c9c717a294
Nick <nlhsueh@gmail.com>
2024-09-18 12:44:20 +0800

    .

---
+++ lab/u01_debug/GCD.java
@@ -0,0 +1,27 @@
+package xdemo;
+
+/**
+ * Use break/watch to debug and watch program execution
+ */
+public class GCD {
+
+    public static void main(String[] args) {
+        int r;
+        GCD g = new GCD();
+        r = g.gcd(24, 18);
+        System.out.println(r);
+
+        r = g.gcd2(24, 18);
+        System.out.println(r);
+    }
+
+    public int gcd(int m, int n) {
+        // using while loop
+
+    }
+
+    public int gcd2(int m, int n) {
+        // using recursive
+
+    }
+}
