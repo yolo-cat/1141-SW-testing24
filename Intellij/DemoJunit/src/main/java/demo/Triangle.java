8790b48e61ec1ec8082df807a1d02368468b9a93
nienlinh <nlhsueh@mail.fcu.edu.tw>
2024-11-25 22:22:27 +0800

    update

--- Intellij/DemoJunit/src/main/java/demo/Triangle.java
+++ Intellij/DemoJunit/src/main/java/demo/Triangle.java
@@ -8,22 +8,22 @@ package demo;
 
 public class Triangle {
     public static String getTriangleType(double a, double b, double c) {
-            if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
-                return "Not a valid triangle";
-            }
-
-            // Check for equilateral triangle
-            if (a == b && b == c & c==a) {
-                return "Equilateral";
-            }
-
-            // Check for isosceles triangle
-            if (a == b || b == c || a == c) {
-                return "Isosceles";
-            }
-
-            // Otherwise, it's a scalene triangle
-            return "Scalene"; // 不等邊三角形
+        if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+            return "Not a valid triangle";
         }
+
+        // Check for equilateral triangle
+        if (a == b && b == c && c == a) {
+            return "Equilateral";
+        }
+
+        // Check for isosceles triangle
+        if (a == b || b == c || a == c) {
+            return "Isosceles";
+        }
+
+        // Otherwise, it's a scalene triangle
+        return "Scalene"; // 不等邊三角形
+    }
 
 }
